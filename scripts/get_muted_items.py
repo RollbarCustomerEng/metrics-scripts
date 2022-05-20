7c41c7e3475beb15c4469b639877f5ab6424c3d4
finbarfleming <finbar.fleming@gmail.com>
2022-05-20 10:05:40 -0400

    initial commit for getting list of muted items that are currently occurring

---
+++ scripts/get_muted_items.py
@@ -0,0 +1,19 @@
+import os
+
+def get_project_access_tokens():
+
+    os.environ['HOME']
+
+
+
+def get_token_file_path():
+    # Returns: file path of csv file with the project_name, read_access_tokens
+    
+    os.environ.get('ROLLBAR_TOKEN_FILE_PATH')
+
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
