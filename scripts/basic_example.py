344f5857ae95de51279c26da261de78f3fe66f55
finbar-rollbar <finbar.fleming@rollbar.com>
2022-10-19 16:38:01 -0400

    make quote style consistent. get the data for last 14 days

--- scripts/basic_example.py
+++ scripts/basic_example.py
@@ -1,34 +1,44 @@
 import os
 from tracemalloc import start
 import requests
-import datetime
+import time
 
 url = "https://api.rollbar.com/api/1/metrics/occurrences"
-project_token = os.environ['ROLLBAR_PROJECT_READ_ACCESS_TOKEN']
+project_token = os.environ["ROLLBAR_PROJECT_READ_ACCESS_TOKEN"]
 
 # https://www.epochconverter.com/
 
 
-start_time = 1662033600
-end_time = start_time + (1 * 24 * 60 * 60) # 1665587613
+   
+end_time = int(time.time())
+start_time = end_time - (14 * 24 * 60 * 60)
 
 
 payload = {
     "start_time": start_time,
     "end_time": end_time,
     "filters": [{"field": "item_level",
-                 "values": ['error', 'critical'],
+                 "values": ["error"],
+                 "operator": "eq"
+                 },
+                 {"field": "environment",
+                 "values": ["production"],
                  "operator": "eq"
                  }
+
                 ],
-    "group_by": ["environment", "item_level"],
-    "granularity": "hour",
-    'aggregates': [
+                "group_by": ["code_version", "item_level"],
+                "aggregates": [
                     {
-                        'field': 'person_id',
-                        'function': 'count_distinct',
-                        'alias': 'person_count'
-                    }
+                        "field": "item_counter",
+                        "function": "count_distinct",
+                        "alias": "item_count"
+                    },
+                    {
+                        "field": "person_id",
+                        "function": "count_distinct",
+                        "alias": "person_count"
+                    },
                   ]
     }
 
@@ -42,6 +52,6 @@ response = requests.post(url, json=payload, headers=headers)
 
 print(response.text)
 
-f = open('out.csv', 'w')
+f = open("out.json", "w")
 f.write(response.text)
 f.close()
